eca4a9bc7bc132bf0418b1737a8f33081d6c31e8
eklid <eklid.isufaj1@gmail.com>
2022-01-16 21:14:39 +0100

    Search update

--- app.js
+++ app.js
@@ -23,3 +23,59 @@ function onClick() {
   document.getElementsByTagName("body")[0].style.backgroundImage =
     'url("tirana2.jpg")';
 }
+
+function onSearch() {
+  let city = document.getElementById("city-search").value;
+
+  if (city == "Tirana") {
+    document.getElementById("location").innerHTML =
+      '<i class="fas fa-map-marker-alt"></i> Tirana, Albania';
+
+    let text = "Tirana \nCity";
+    let text1 = "";
+    let pulsor = "|";
+    for (let i = 0; i < text.length; i++) {
+      setTimeout(() => {
+        text1 += text.charAt(i);
+        document.getElementById("city").innerText = text1 + pulsor;
+      }, 300 * i);
+    }
+    document.getElementById("gradeCelcius").innerHTML =
+      '<i class="fas fa-sun"></i> 25&#8451;';
+
+    document.getElementById("distance").innerHTML =
+      '<i class="fas fa-map-marker-alt"></i> Distance: 51m';
+
+    document.getElementById("duration").innerHTML =
+      '<i class="fas fa-stopwatch"></i> Duration: 10h';
+
+    document.getElementsByTagName("body")[0].style.backgroundImage =
+      'url("tirana2.jpg")';
+  }
+
+  if (city == "Durres") {
+    document.getElementById("location").innerHTML =
+      '<i class="fas fa-map-marker-alt"></i> Durres, Albania';
+
+    let text = "Durres \nCity";
+    let text1 = "";
+    let pulsor = "|";
+    for (let i = 0; i < text.length; i++) {
+      setTimeout(() => {
+        text1 += text.charAt(i);
+        document.getElementById("city").innerText = text1 + pulsor;
+      }, 300 * i);
+    }
+    document.getElementById("gradeCelcius").innerHTML =
+      '<i class="fas fa-sun"></i> 29&#8451;';
+
+    document.getElementById("distance").innerHTML =
+      '<i class="fas fa-map-marker-alt"></i> Distance: 151m';
+
+    document.getElementById("duration").innerHTML =
+      '<i class="fas fa-stopwatch"></i> Duration: 5h';
+
+    document.getElementsByTagName("body")[0].style.backgroundImage =
+      'url("tirana2.jpg")'; //ndrysho foto
+  }
+}
